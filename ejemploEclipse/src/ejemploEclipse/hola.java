27031ffe28cd63e82dfe0c852069beb42aedbae3
JoseAka <nouse1998@gmail.com>
2021-03-24 17:39:11 +0100

    quitar me has escuchado

--- ejemploEclipse/src/ejemploEclipse/hola.java
+++ ejemploEclipse/src/ejemploEclipse/hola.java
@@ -4,6 +4,6 @@ public class hola {
 	public static void main(String[] args) {
 		System.out.println("Hola");
 		System.out.println("Que tal?");
-		System.out.println("Me has escuchado?");
+		
 	}
 }
